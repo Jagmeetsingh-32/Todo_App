048e41f417438e45bf6325c4e0f790384ee37ecd
Jagmeetsingh-32 <jagmeetsingh3095@gmail.com>
2025-06-30 16:43:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) 
 
 # Define Todo model
 class Todo(db.Model):
